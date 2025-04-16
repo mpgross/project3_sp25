b0c1169c7dbbb271a3cef37512bbcf24f4ad86c1
Max G. <66702601+s1idewhist1e@users.noreply.github.com>
2025-04-15 22:06:42 -0600

    Do the project :3

--- rsh.c
+++ rsh.c
@@ -1,46 +1,117 @@
+#include <spawn.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <spawn.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <string.h>
+
+static int builtin_cd(char **argv, int argc);
+static int builtin_exit(char **argv, int argc);
+static int builtin_help(char **argv, int argc);
 
 #define N 12
 
 extern char **environ;
 
-char *allowed[N] = {"cp","touch","mkdir","ls","pwd","cat","grep","chmod","diff","cd","exit","help"};
+char *allowed[N] = {"cp",   "touch", "mkdir", "ls", "pwd",  "cat",
+                    "grep", "chmod", "diff",  "cd", "exit", "help"};
 
-int isAllowed(const char*cmd) {
-	// TODO
-	// return 1 if cmd is one of the allowed commands
-	// return 0 otherwise
-	
-	return 0;
+// An array of builtins corresponding to commands
+int (*builtins[N])(char **, int) = {
+    NULL, NULL, NULL, NULL,       NULL,         NULL,
+    NULL, NULL, NULL, builtin_cd, builtin_exit, builtin_help};
+
+int isAllowed(const char *cmd) {
+  for (int i = 0; i < N; ++i) {
+    if (!strcmp(cmd, allowed[i])) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 int main() {
 
-    // TODO
-    // Add variables as needed
+  char *arguments[21] = {NULL};
 
-    char line[256];
+  char line[256];
 
-    while (1) {
+  while (1) {
 
-	fprintf(stderr,"rsh>");
+    // State for strtok_r because I can
+    char *save_ptr = NULL;
 
-	if (fgets(line,256,stdin)==NULL) continue;
+    fprintf(stderr, "rsh>");
 
-	if (strcmp(line,"\n")==0) continue;
+    if (fgets(line, 256, stdin) == NULL)
+      continue;
 
-	line[strlen(line)-1]='\0';
+    if (strcmp(line, "\n") == 0)
+      continue;
 
-	// TODO
-	// Add code to spawn processes for the first 9 commands
-	// And add code to execute cd, exit, help commands
-	// Use the example provided in myspawn.c
+    line[strlen(line) - 1] = '\0';
 
+    int argc = 0;
+    arguments[argc] = strtok_r(line, " ", &save_ptr);
+    while ((arguments[++argc] = strtok_r(NULL, " ", &save_ptr)) != NULL &&
+           argc < 20)
+      ;
+
+    arguments[20] = NULL;
+
+    int command_idx;
+    if ((command_idx = isAllowed(arguments[0])) == -1) {
+      puts("NOT ALLOWED!");
+      continue;
+    } else if (command_idx < 9) {
+      // Spawn process and wait for it to complete
+      pid_t pid;
+      posix_spawnattr_t attr;
+      posix_spawnattr_init(&attr);
+
+      // spawn the process
+      posix_spawnp(&pid, arguments[0], NULL, &attr, arguments, environ);
+
+      // wait for it to finish
+      int status;
+      waitpid(pid, &status, 0);
+
+      posix_spawnattr_destroy(&attr);
+      continue;
+    } else if (command_idx < N) {
+      builtins[command_idx](arguments, argc);
+      continue;
+    } else {
+      puts("Unreachable!?");
+      exit(-1);
     }
-    return 0;
+
+    // TODO
+    // Add code to spawn processes for the first 9 commands
+    // And add code to execute cd, exit, help commands
+    // Use the example provided in myspawn.c
+  }
+  return 0;
+}
+
+int builtin_cd(char **argv, int argc) {
+
+  if (argc != 2) {
+    puts("-rsh: cd: too many arguments");
+    return -1;
+  }
+
+  chdir(argv[1]);
+
+  return 0;
+}
+
+int builtin_exit(char **argv, int argc) { exit(0); }
+
+int builtin_help(char **argv, int argc) {
+  puts("The allowed commands are:");
+  for (int i = 0; i < N; i++) {
+    printf("%d: %s\n", i + 1, allowed[i]);
+  }
+  return 0;
 }
